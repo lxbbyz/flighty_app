23b6bb4970349b8f0800014d1575862f58a407e4
greenfir <m18797997521_1@163.com>
2019-05-11 01:38:33 +0800

    webscoket

---
+++ pages/pairing/pairing.vue
@@ -0,0 +1,210 @@
+<template>
+	<view>
+		<!-- <view class="uni-padding-wrap uni-common-mt">
+			<view class="animation-element-wrapper">
+				<view class="animation-element" :animation="animationData"></view>
+				<view class="animation-element1" :animation="animationData1"></view>
+				<view class="animation-element2" :animation="animationData2"></view>
+				<view class="animation-element3" :animation="animationData3"></view>
+				<view class="animation-element4" :animation="animationData4"></view>
+				<view class="animation-element5" :animation="animationData5"></view>
+				<view class="animation-element6" :animation="animationData6"></view>
+				<view class="animation-element7" :animation="animationData7"></view>
+			</view>
+			<scroll-view class="animation-buttons" scroll-y="true">
+				<button class="animation-button" @tap="rotate">旋转</button>
+			</scroll-view>
+		</view> -->
+		<view style="display: flex;justify-content: center;flex-direction: row;">
+			<button :disabled="symbolDis" @click="operation(symbol1)" style="width: 150upx;height: 150upx;border: thin solid #00C777;margin: 30upx;border-radius: 30upx;line-height: 150upx;">A</button>
+			<button :disabled="numberDis" @click="operation(number1,'numH1')" style="width: 150upx;height: 150upx;border: thin solid #00C777;margin: 30upx;border-radius: 30upx;line-height: 150upx;">B</button>
+			<button :disabled="symbolDis" @click="operation(symbol2)" style="width: 150upx;height: 150upx;border: thin solid #00C777;margin: 30upx;border-radius: 30upx;line-height: 150upx;">C</button>
+		</view>
+		<view style="display: flex;justify-content: center;flex-direction: row;">
+			<button :disabled="numberDis" @click="operation(number2,'numH2')" style="width: 150upx;height: 150upx;border: thin solid #00C777;margin: 30upx;border-radius: 30upx;line-height: 150upx;">D</button>
+			<button style="width: 150upx;height: 150upx;border: thin solid #00C777;margin: 30upx;border-radius: 30upx;"></button>
+			<button :disabled="numberDis" @click="operation(number3,'numH3')" style="width: 150upx;height: 150upx;border: thin solid #00C777;margin: 30upx;border-radius: 30upx;line-height: 150upx;">E</button>
+		</view>
+		<view style="display: flex;justify-content: center;flex-direction: row;">
+			<button :disabled="symbolDis" @click="operation(symbol3)" style="width: 150upx;height: 150upx;border: thin solid #00C777;margin: 30upx;border-radius: 30upx;line-height: 150upx;">F</button>
+			<button :disabled="numberDis" @click="operation(number4,'numH4')" style="width: 150upx;height: 150upx;border: thin solid #00C777;margin: 30upx;border-radius: 30upx;line-height: 150upx;">G</button>
+			<button :disabled="symbolDis" @click="operation(symbol4)" style="width: 150upx;height: 150upx;border: thin solid #00C777;margin: 30upx;border-radius: 30upx;line-height: 150upx;">H</button>
+		</view>
+		
+		<view style="margin-top: 400upx;">{{formula}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				numberDis:false,
+				symbolDis:true,
+				symbol1:'+',
+				symbol2:'-',
+				symbol3:'*',
+				symbol4:'/',
+				number1:1,
+				number2:2,
+				number3:3,
+				number4:4,
+				formula:'',	//算式
+				animationData: '',
+				animationData1:'',
+				animationData2:'',
+				animationData3:'',
+				animationData4:'',
+				animationData5:'',
+				animationData6:'',
+				animationData7:''
+			}
+		},
+		onLoad() {
+			this.animation = uni.createAnimation({
+				duration: 3000,
+				timingFunction: 'ease',
+			});
+			this.animation1 = uni.createAnimation({
+				duration: 3000,
+				timingFunction: 'ease',
+			});
+			this.animation2 = uni.createAnimation({
+				duration: 3000,
+				timingFunction: 'ease',
+			});
+			this.animation3 = uni.createAnimation({
+				duration: 3000,
+				timingFunction: 'ease',
+			});
+			this.animation4 = uni.createAnimation({
+				duration: 3000,
+				timingFunction: 'ease',
+			});
+			this.animation5 = uni.createAnimation({
+				duration: 3000,
+				timingFunction: 'ease',
+			});
+			this.animation6 = uni.createAnimation({
+				duration: 3000,
+				timingFunction: 'ease',
+			});
+			this.animation7 = uni.createAnimation({
+				duration: 3000,
+				timingFunction: 'ease',
+			});
+		},
+		onShow: function(){
+			
+		},
+		methods: {
+			operation(str,numberHid){
+				console.log(numberHid);
+				if(str>0){
+					this.numberDis = true;
+					this.symbolDis = false;
+					this.formula = this.formula + str;
+				}else{
+					this.numberDis = false;
+					this.symbolDis = true;
+					this.formula = this.formula + str;
+				}
+			},
+			
+			rotate: function () {
+				// this.animation.rotate3d(1,100,0,3600).step()
+				
+				this.animation.rotate(3600).step().translateX(100).step();
+				this.animationData = this.animation.export()
+				
+				this.animation1.rotate(3600).step().translateY(100).step();
+				this.animationData1 = this.animation1.export()
+				
+				this.animation2.rotate(3600).step().translateX(-100).step();
+				this.animationData2 = this.animation2.export()
+				
+				this.animation3.rotate(3600).step().translateY(-100).step();
+				this.animationData3 = this.animation3.export()
+				
+				this.animation4.rotate(3600).step().translate3d(70,70,70).step();
+				this.animationData4 = this.animation4.export()
+				
+				this.animation5.rotate(3600).step().translate3d(-70,70,70).step();
+				this.animationData5 = this.animation5.export()
+				
+				this.animation6.rotate(3600).step().translate3d(70,-70,70).step();
+				this.animationData6 = this.animation6.export()
+				
+				this.animation7.rotate(3600).step().translate3d(-70,-70,-70).step();
+				this.animationData7 = this.animation7.export()
+				
+			}
+		}
+	}
+</script>
+
+<style>
+	.animation-element-wrapper {
+		display: flex;
+		width: 100%;
+		padding-top: 300upx;
+		padding-bottom: 300upx;
+		justify-content: center;
+		overflow: hidden;
+		background-color: #ffffff;
+	}
+	.animation-element {
+		width: 100upx;
+		height: 100upx;
+		background-color: #1AAD19;
+		position: absolute;
+	}
+	.animation-element1 {
+		width: 100upx;
+		height: 100upx;
+		background-color: red;
+		position: absolute;
+	}
+	
+	.animation-element2{
+		width: 100upx;
+		height: 100upx;
+		background-color: blue;
+		position: absolute;
+	}
+	
+	.animation-element3{
+		width: 100upx;
+		height: 100upx;
+		background-color: yellow;
+		position: absolute;
+	}
+	
+	.animation-element4{
+		width: 100upx;
+		height: 100upx;
+		background-color: #576B95;
+		position: absolute;
+	}
+	
+	.animation-element5{
+		width: 100upx;
+		height: 100upx;
+		background-color: #66AAFF;
+		position: absolute;
+	}
+	
+	.animation-element6{
+		width: 100upx;
+		height: 100upx;
+		background-color: #E96900;
+		position: absolute;
+	}
+	
+	.animation-element7{
+		width: 100upx;
+		height: 100upx;
+		background-color: #4CD964;
+		position: absolute;
+	}
+</style>
--- pages/tabBar/game/game.vue
+++ pages/tabBar/game/game.vue
@@ -1,14 +1,34 @@
 <template>
 	<view>
 		<!-- <uni-grid :options="params" column-num="2" type="square" @click="goTo"></uni-grid> -->
-		<button @click="goTo">连接</button>
+		
+		<button @click="goTo(10)">连接1</button>
+		内容：<input style="border: thin solid #00C777;" type="text" v-model="textOne" />
+		<button @click="sendMessage(10,20,textOne)" style="width: 200upx;height: 100upx;line-height: 100upx;left: 0;position: absolute;">发送</button>
+		<view v-for="(item, index1) in contentList1" :key="index1" style="margin-top: 110upx;">
+			<!-- 自己 -->
+			<text style="display: flex;justify-content: flex-end;" v-if="item.formUserSid===10">{{item.formUserSid}}:{{item.content}}</text>
+			<!-- 他人 -->
+			<text v-else>{{item.formUserSid}}:{{item.content}}</text>
+		</view>
+		
+		<button @click="goTo(20)" style="margin-top: 400upx;">连接2</button>
+		内容：<input style="border: thin solid #00C777;" type="text" v-model="textTwo" />
+		<button @click="sendMessage(20,10,textTwo)" style="width: 200upx;height: 100upx;line-height: 100upx;left: 0;position: absolute;">发送</button>
+		<view v-for="(item, index2) in contentList2" :key="index2" style="margin-top: 110upx;">
+			<!-- 自己 -->
+			<text style="display: flex;justify-content: flex-end;" v-if="item.formUserSid===20">{{item.formUserSid}}:{{item.content}}</text>
+			<!-- 他人 -->
+			<text v-else>{{item.formUserSid}}:{{item.content}}</text>
+		</view>
+		
 	</view>
 </template>
 
 <script>
 	import uniGrid from "@/components/uni-grid/uni-grid.vue"
-	import store from "@/components/uni-app-chat/store.js"
-	import socket from "@/components/uni-app-chat/socket.js"
+	// import store from "@/components/uni-app-chat/store.js"
+	// import socket from "@/components/uni-app-chat/socket.js"
 	
 	export default {
 		data() {
@@ -18,36 +38,70 @@
 					{image:'https://img-cdn-qiniu.dcloud.net.cn/img/lindang.png',text:'铃铛'},
 					{image:'https://img-cdn-qiniu.dcloud.net.cn/img/laoren.png',text:'圣诞老人'},
 					{image:'https://img-cdn-qiniu.dcloud.net.cn/img/liwu.png',text:'礼物'}
-				]
+				],
+				textOne:'',
+				textTwo:'',
+				userOne:'20',
+				userTwo:'10',
+				contentList1:[],
+				contentList2:[]
 			}
 		},
 		components:{
-			uniGrid,
-			store,
-			socket
+			uniGrid
 		},
 		methods: {
+			//发送消息
+			sendMessage(formUserSid,toUserSid,content){
+				let message = {
+					formUserSid:formUserSid,
+					toUserSid:toUserSid,
+					content:content
+				}
+				if(formUserSid===10){
+					this.contentList1.push(message);
+				}else{
+					this.contentList2.push(message);
+				}
+				console.log("消息-->"+JSON.stringify(message));
+				this.sendSocketMessage(JSON.stringify(message));
+				// uni.navigateTo({
+				// 	url:"../../pairing/pairing"
+				// })
+			},
+			//连接webscoket
 			goTo(data_) {
+				let that = this;
 				console.log(data_.index);
-				uni.setStorageSync('userToken', 'oiWGP4vo8erpTtL6U5s581A7M-eE');
-				
-				// ws://47.99.99.79:8083/shop//websocket/socketServer.do
-				// ws://192.168.0.126:9080/flighty/websocket/socketServer.do?SESSION_USERNAME=15170688099
+				// uni.setStorageSync('userToken', 'oiWGP4vo8erpTtL6U5s581A7M-eE');
 				
 				uni.connectSocket({
-					url: 'ws://192.168.0.126:9080/flighty/websocket/20',
-					success(e) {
-						console.log("返回的数据-->"+JSON.stringify(e));
-					},
-					fail(e) {
-						console.log("返回的数据-->"+JSON.stringify(e));
-					}
+					url: 'ws://192.168.0.126:9080/flighty/websocket/'+data_
 				});
 				uni.onSocketOpen(function (res) {
 					console.log('WebSocket连接已打开！');
 				});
 				uni.onSocketError(function (res) {
 					console.log('WebSocket连接打开失败，请检查！');
+				});
+				uni.onSocketMessage(function (res) {
+					console.log('收到服务器内容：' + res.data);
+					console.log(JSON.parse(res.data).toUserSid);
+					let message = {
+						formUserSid:JSON.parse(res.data).formUserSid,
+						toUserSid:JSON.parse(res.data).toUserSid,
+						content:JSON.parse(res.data).content
+					}
+					if(JSON.parse(res.data).toUserSid===10){
+						that.contentList1.push(message);
+					}else{
+						that.contentList2.push(message);
+					}
+				});
+			},
+			sendSocketMessage(msg) {
+				uni.sendSocketMessage({
+					data: msg
 				});
 			}
 		}
